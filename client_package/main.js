a37016274c7f5aa5e696158034d15b9083a48f04
noobasaurus <rainbowdatabot@yahoo.com>
2017-07-01 13:12:39 -0700

    init

---
+++ client_package/main.js
@@ -0,0 +1,4 @@
+const ui = new WebUIWindow('test-mouse', 'package://test-mouse/ui/index.html', new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
+ui.autoResize = true;
+ui.hidden = false;
+    
